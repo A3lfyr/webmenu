6e507c29c8e0270da571ed54a8531a9011e46bcc
arthur-reiter <32246982+arthur-reiter@users.noreply.github.com>
2020-08-27 10:50:46 +0200

    try something

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -11,7 +11,7 @@ var vue_welcomeMessage = new Vue({
     },
     methods: {
         getRandomQuote: function () {
-            fetch("http://kaamelott.reiter.tf/api/random")
+            fetch("https://kaamelott.reiter.tf/api/random")
                 .then(result => {
                     return result.json();
                 })
