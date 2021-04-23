2209ded4516164aab8e8e60ac1bc300021728bc4
Arthur REITER <arthur.reiter231@gmail.com>
2021-04-23 13:29:26 +0200

    fix KaamelottAPI URL

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -11,7 +11,7 @@ var vue_welcomeMessage = new Vue({
     },
     methods: {
         getRandomQuote: function () {
-            fetch("https://www.kaamelott.reiter.tf/api/random")
+            fetch("https://kaamelott.reiter.tf/quote/random")
                 .then(result => {
                     return result.json();
                 })
