14409c7bcb120f33f220c2ace57799554ea260eb
Arthur REITER <arthur.reiter231@gmail.com>
2021-05-01 12:05:34 +0200

    Update background list

--- src/components/background.js
+++ src/components/background.js
@@ -7,86 +7,97 @@ var vue_background = new Vue({
             author: null
         },
         theme: null,
-        backgrounds: [{
-            "url": "https://images.unsplash.com/photo-1516528387618-afa90b13e000?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
+        backgrounds: [
+        {
+            "url": "https://images.unsplash.com/photo-1516528387618-afa90b13e000?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
             "link": "https://unsplash.com/photos/jqgsM3B9Fpo",
-            "user": "Yoal Desurmont"
+            "user": "Yoal Desurmont",
+            "site:": "Unsplash"
         },
         {
-            "url": "https://images.unsplash.com/photo-1483631224226-a219224bb76e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/sMJaf08ugD0",
-            "user": "Nicolas Prieto"
+            "url": "https://images.pexels.com/photos/1072179/pexels-photo-1072179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/feuilles-vertes-1072179/",
+            "user": "Cátia Matos",
+            "site:": "Pexel"
         },
         {
-            "url": "https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/M6XC789HLe8",
-            "user": "Aniket Deole"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/eOpewngf68w",
-            "user": "Tim Swaan"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1582341305248-af5d85a9c0cd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/_TSHXXo52hA",
-            "user": "Nik Shuliahin"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/rCbdp8VCYhQ",
-            "user": "Andy Holmes"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1567544568263-55b6f67b5289?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/10kjsFg6M4E",
-            "user": "Andy Holmes"
+            "url": "https://images.unsplash.com/photo-1531156992292-d36397ee9184?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
+            "link": "https://unsplash.com/photos/_1UF_3TlKcQ",
+            "user": "Mykyta Martynenko",
+            "site:": "Unsplash"
         },
         {
             "url": "https://images.unsplash.com/photo-1566050980594-f65e237a2e98?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
             "link": "https://unsplash.com/photos/4iapyjvm714",
-            "user": "Andy Holmes"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1505370914932-3d5df10b0d4e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/zWRgJjM5GhE",
-            "user": "Ivan Torres"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1589112028183-ec50fde02586?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/jn2rJK-NEDg",
-            "user": "DICSON"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1458780981405-16d00e96b523?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/Bc9DPNExOV8",
-            "user": "Jamie Street"
+            "user": "Andy Holmes",
+            "site:": "Unsplash"
         },
         {
             "url": "https://images.unsplash.com/photo-1580218102234-490e83efd85b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
             "link": "https://unsplash.com/photos/HI5rD5zbU2o",
-            "user": "Willian Justen de Vasconcellos"
+            "user": "Willian Justen de Vasconcellos",
+            "site:": "Unsplash"
         },
         {
             "url": "https://images.unsplash.com/photo-1597661905942-8d50e4ab6fc8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
             "link": "https://unsplash.com/photos/_F2T1OyB7K0",
-            "user": "Annie Spratt"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1592526985927-119090617768?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/8_VWBXsveQE",
-            "user": "Nick Fewings"
-        },
-        {
-            "url": "https://images.unsplash.com/photo-1597287258659-0b6391c0c5f5?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
-            "link": "https://unsplash.com/photos/NQELNMBFt1U",
-            "user": "Alex H. Pflaum"
+            "user": "Annie Spratt",
+            "site:": "Unsplash"
         },
         {
             "url": "https://images.unsplash.com/photo-1578915424189-f3d4297b385a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyNjI0MH0",
             "link": "https://unsplash.com/photos/jJ2pHxn5mXw",
-            "user": "Federico Bottos"
-        }
+            "user": "Federico Bottos",
+            "site:": "Unsplash"
+        },
+        {
+            "url": "https://images.pexels.com/photos/3875821/pexels-photo-3875821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/vue-aerienne-des-batiments-de-la-ville-3875821/",
+            "user": "Peter Olexa",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/4512439/pexels-photo-4512439.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/volant-de-voiture-noir-pendant-la-nuit-4512439/",
+            "user": "Aleksandar Pasaric",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/4004374/pexels-photo-4004374.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/collection-de-plumes-delicates-sur-fond-noir-4004374/",
+            "user": "Evie Shaffer",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/photo-d-horizon-la-nuit-3075993/",
+            "user": "Sanaan Mazhar",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/personnes-au-concert-1105666/",
+            "user": "Vishnu R Nair",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/1114900/pexels-photo-1114900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/eclipse-lunaire-1114900/",
+            "user": "Johannes Plenio",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/montagnes-derriere-la-pleine-lune-1183021/",
+            "user": "Eberhard Grossgasteiger",
+            "site:": "Pexel"
+        },
+        {
+            "url": "https://images.pexels.com/photos/949193/pexels-photo-949193.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "link": "https://www.pexels.com/fr-fr/photo/homme-en-veste-rouge-debout-a-l-exterieur-de-la-grotte-en-face-de-trois-montagnes-949193/",
+            "user": "S Migaj",
+            "site:": "Pexel"
+        },
     ],
     },
     methods: {
