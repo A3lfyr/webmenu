99522de951bb5787266c78ef4f3bd2964831ec48
GabJimz <46873801+GabJimz@users.noreply.github.com>
2021-05-02 19:49:36 +0200

    Clock component creation functional

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>{{msg}}</p>
+  <h1 class="title" id="time" v-if="enable">{{ time }}</h1>
 </template>
 
 <script>
@@ -7,16 +7,37 @@ export default {
   name: 'Clock',
   data() {
     return {
-      msg: "Clock"
+      enable: true,
+      time: ""
     };
   },
   methods: {
-    removePolldleOption() {
+    getTime: function() {
+        const today = new Date();
+        this.time = "";
+        if (today.getHours() < 10) {
+            this.time += "0";
+        }
+        this.time += today.getHours() + ":";
+        if (today.getMinutes() < 10) {
+            this.time += "0";
+        }
+        this.time += today.getMinutes();
     }
+  },
+  created() {
+    setInterval(() => {
+      this.getTime()
+    }, 100);
   }
 }
 </script>
 
 <style scoped>
-
+  h1 {
+    font-size: 10em;
+    color: white;
+    font-family: 'Titillium Web', sans-serif;
+    font-weight: 300;
+  }
 </style>
